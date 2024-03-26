fc40deeffa89402be01da1dcab1760cc56ce8442
breandan <bre@ndan.co>
2024-03-26 00:45:39 -0400

    add endpoint to serve BIFI breaker

--- src/serve_model.py
+++ src/serve_model.py
@@ -6,10 +6,16 @@ from fairseq import tokenizer
 
 app = Flask(__name__)
 
-trans = TransformerModel.from_pretrained(
+breaker = TransformerModel.from_pretrained(
+  'data/round2-BIFI-part1/model-breaker/',
+  checkpoint_file='checkpoint.pt',
+  data_name_or_path='data/round2-BIFI-part2/orig_bad/fairseq_preprocess__orig_good.9',
+)
+
+fixer = TransformerModel.from_pretrained(
   'data/round2-BIFI-part2/model-fixer/',
   checkpoint_file='checkpoint.pt',
-  data_name_or_path='data/round2-BIFI-part2/orig_bad/fairseq_preprocess__orig_bad.0',
+  data_name_or_path='data/round2-BIFI-part2/orig_bad/fairseq_preprocess__orig_bad.4',
 )
 
 @app.route('/api/text', methods=['GET'])
@@ -18,23 +24,28 @@ def get_text():
     url_tok = request.args.get('bifi_tokenize')
     url_topk = request.args.get('bifi_topk')
     get_k = request.args.get('k')
+    url_break = request.args.get('bifi_break')
     if url:
         decoded_url = urllib.parse.unquote(url)
-        output = trans.translate(decoded_url, beam=10, verbose=True)
+        output = fixer.translate(decoded_url, beam=10, verbose=True)
         return output
     elif url_tok:
         decoded_url = urllib.parse.unquote(url_tok)
-        output = trans.decode(trans.encode(decoded_url))
+        output = fixer.decode(fixer.encode(decoded_url))
         return output
     elif url_topk:
         decoded_url = urllib.parse.unquote(url_topk)
         k = int(get_k)
         # Retrieve the top 10 most likely token sequences
         # output = trans.generate(decoded_url, beam=10, verbose=True)
-        concat = trans.sample_topk([decoded_url], beam=k, verbose=True)
+        concat = fixer.sample_topk([decoded_url], beam=k, verbose=True)
         str_concat = '\n'.join(concat)
         return str_concat
+    elif url_break:
+        decoded_url = urllib.parse.unquote(url_break)
+        output = breaker.translate(decoded_url, beam=10, verbose=True)
+        return output
 
-# http://localhost:5000/api/text?bifi_translate=def%20foo%28%29%3A%0A%20%20%20%20print%28%22Hello%2C%20World%21%22%29
+# http://localhost:5000/api/text?bifi_break=test
 if __name__ == '__main__':
     app.run()
