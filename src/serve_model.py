df801208ac7ddbd6e71e9fc360f25c1160a99a13
breandan <bre@ndan.co>
2024-03-25 22:15:30 -0400

    add support for top-k sampling

--- src/serve_model.py
+++ src/serve_model.py
@@ -1,6 +1,7 @@
 from fairseq.models.transformer import TransformerModel
 import urllib.parse
 from flask import Flask, request
+from fairseq import tokenizer
 
 
 app = Flask(__name__)
@@ -13,11 +14,27 @@ trans = TransformerModel.from_pretrained(
 
 @app.route('/api/text', methods=['GET'])
 def get_text():
-    url = request.args.get('bifi')
-    decoded_url = urllib.parse.unquote(url)
-    output = trans.translate(decoded_url)
-    return output
+    url = request.args.get('bifi_translate')
+    url_tok = request.args.get('bifi_tokenize')
+    url_topk = request.args.get('bifi_topk')
+    get_k = request.args.get('k')
+    if url:
+        decoded_url = urllib.parse.unquote(url)
+        output = trans.translate(decoded_url, beam=10, verbose=True)
+        return output
+    elif url_tok:
+        decoded_url = urllib.parse.unquote(url_tok)
+        output = trans.decode(trans.encode(decoded_url))
+        return output
+    elif url_topk:
+        decoded_url = urllib.parse.unquote(url_topk)
+        k = int(get_k)
+        # Retrieve the top 10 most likely token sequences
+        # output = trans.generate(decoded_url, beam=10, verbose=True)
+        concat = trans.sample_topk([decoded_url], beam=k, verbose=True)
+        str_concat = '\n'.join(concat)
+        return str_concat
 
-
+# http://localhost:5000/api/text?bifi_translate=def%20foo%28%29%3A%0A%20%20%20%20print%28%22Hello%2C%20World%21%22%29
 if __name__ == '__main__':
     app.run()
